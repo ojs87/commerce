3c3ce6994c5265a8eac459d7a29521110e1850dd
ojs87 <73496902+ojs87@users.noreply.github.com>
2020-11-27 17:19:33 +0000

    initial commit

---
+++ auctions/models.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django import forms
+
+
+class User(AbstractUser):
+    watchlist = models.ManyToManyField('Auction')
+
+class Auction(models.Model):
+    name = models.CharField(max_length=64)
+    url = models.URLField(max_length=500)
+    CHOICES = [
+        ('FA', 'Fashion'),
+        ('HG', 'Home & Garden'),
+        ('EL', 'Electronics'),
+        ('HE', 'Home Entertainment'),
+        ('TO', 'Toys'),
+        ('CO', 'Collectables')
+    ]
+    categories = models.CharField(choices=CHOICES, max_length=2)
+    description = models.CharField(max_length=500)
+
+class Bid(models.Model):
+    auction = models.ForeignKey('Auction', on_delete=models.CASCADE, related_name="highestbid")
+    user=models.ForeignKey('User', on_delete=models.CASCADE, related_name="bidowner")
+    bidvalue=models.DecimalField(max_digits=7, decimal_places=2)
